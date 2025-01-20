e3d3d8379dfc2e4c05124ea8bea043ed176cc109
Jyotirmoy <jyotirmoydas12@gmail.com>
2025-01-20 23:04:46 +0530

    update baseUrls

--- client/src/api/axiosInstance.js
+++ client/src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "https://test-new-hlwm.onrender.com",
+  baseURL: "https://test-new-x91y.onrender.com",
 });
 
 axiosInstance.interceptors.request.use(
